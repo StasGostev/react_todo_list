fddf6a262ef956d5cd8b55378be4dc8911f5b1a4
StasGostev <spaceomen@gmail.com>
2019-11-18 17:16:30 +0200

    Updates

---
+++ precache-manifest.344f6008354cf23ecfdc0f365be79fe9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98fc0887e2d144933d73dfdde2a64e4",
+    "url": "/react_todo_list/index.html"
+  },
+  {
+    "revision": "9b37bf05d6c0d9324f16",
+    "url": "/react_todo_list/static/css/main.3c7b89ed.chunk.css"
+  },
+  {
+    "revision": "5490678806bbf1293699",
+    "url": "/react_todo_list/static/js/2.47bb7c75.chunk.js"
+  },
+  {
+    "revision": "9b37bf05d6c0d9324f16",
+    "url": "/react_todo_list/static/js/main.b1899c30.chunk.js"
+  },
+  {
+    "revision": "de6ccb64c67aa611d98f",
+    "url": "/react_todo_list/static/js/runtime-main.e31625af.js"
+  }
+]);
